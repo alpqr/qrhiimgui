b25a90cb53011df0b6ccbc6a31432afa2fb7922f
Laszlo Agocs <laszlo.agocs@qt.io>
2020-05-30 19:44:32 +0200

    Move QWindow-based demo to its own subdir
    ...and update to Dear ImGui 1.76.

---
+++ buildshaders.bat
@@ -0,0 +1,5 @@
+qsb --glsl "100 es,120,150" --hlsl 50 -c --msl 12 imgui.vert -o imgui.vert.qsb
+qsb --glsl "100 es,120,150" --hlsl 50 -c --msl 12 imgui.frag -o imgui.frag.qsb
+
+qsb --glsl "100 es,120,150" --hlsl 50 -c --msl 12 color.vert -o color.vert.qsb
+qsb --glsl "100 es,120,150" --hlsl 50 -c --msl 12 color.frag -o color.frag.qsb
--- buildshaders.sh
+++
@@ -1,2 +0,0 @@
-qsb --glsl "100 es,120" --hlsl 50 --msl 12 imgui.vert -o imgui.vert.qsb
-qsb --glsl "100 es,120" --hlsl 50 --msl 12 imgui.frag -o imgui.frag.qsb
